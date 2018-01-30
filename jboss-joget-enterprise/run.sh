f7a24096d3a6de189d98c2a97306703aaa6b24d3
julian <julian@joget.org>
2018-01-30 21:32:51 +0800

    Modified: docker-joget - Modified jboss-joget-enterprise directory permissions and startup arguments to support OpenShift @6.0-SNAPSHOT

--- jboss-joget-enterprise/run.sh
+++ jboss-joget-enterprise/run.sh
@@ -5,5 +5,5 @@ cd /opt/joget/
 ./apache-ant-1.7.1/bin/ant setup -Ddb.host=${MYSQL_HOST} -Ddb.port=${MYSQL_PORT} -Ddb.user=${MYSQL_USER} -Ddb.password=${MYSQL_PASSWORD} -Ddb.name=${MYSQL_DATABASE} -Dprofile.name=default
 
 # start jboss
-export JAVA_OPTS="$JAVA_OPTS-Djava.net.preferIPv4Stack=true -Dwflow.home=/opt/joget/wflow"
+export JAVA_OPTS="-server -Xms64m -Xmx512m -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=org.jboss.byteman -Djava.awt.headless=true -Dwflow.home=/opt/joget/wflow"
 /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0
