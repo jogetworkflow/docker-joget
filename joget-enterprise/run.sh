72eb43157015e3db0967729e2b25cc23338bce12
Ubuntu <ubuntu@ip-172-31-20-156.us-west-1.compute.internal>
2018-03-28 06:20:44 +0000

    Changes for v5

--- joget-enterprise/run.sh
+++ joget-enterprise/run.sh
@@ -17,5 +17,5 @@ cd /opt/joget/
 ./apache-ant-1.7.1/bin/ant setup -Ddb.host=${MYSQL_HOST} -Ddb.port=${MYSQL_PORT} -Ddb.user=${MYSQL_USER} -Ddb.password=${MYSQL_PASSWORD} -Ddb.name=${MYSQL_DATABASE} -Dprofile.name=default
 
 # start tomcat
-./apache-tomcat-8.5.23/bin/catalina.sh run
+./apache-tomcat-8.0.20/bin/catalina.sh run
 
