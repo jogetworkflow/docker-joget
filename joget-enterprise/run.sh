f8ea7ebccc32ae393ff87750dffb14d55dc0e4a1
julian <julian@joget.org>
2020-09-18 15:47:10 +0800

    Modified: docker-joget - Updated tomcat version to 8.5.58 @7.0-SNAPSHOT

--- joget-enterprise/run.sh
+++ joget-enterprise/run.sh
@@ -17,5 +17,5 @@ cd /opt/joget/
 ./apache-ant-1.7.1/bin/ant setup -Ddb.host=${MYSQL_HOST} -Ddb.port=${MYSQL_PORT} -Ddb.user=${MYSQL_USER} -Ddb.password=${MYSQL_PASSWORD} -Ddb.name=${MYSQL_DATABASE} -Dprofile.name=default
 
 # start tomcat
-./apache-tomcat-8.5.41/bin/catalina.sh run
+./apache-tomcat-${TOMCAT_VERSION}/bin/catalina.sh run
 
